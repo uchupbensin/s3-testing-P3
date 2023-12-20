cc82562e41ad5bdbb10760ddc1d4a12b0ed3effa
Uchup <127828353+uchupbensin@users.noreply.github.com>
2023-12-20 23:46:24 +0700

    Add files via upload

---
+++ CalculatorConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorMachine;
+
+namespace CalculatorConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Demo unit Testing Menggunakan App Console";
+            Calculator cal = new Calculator();
+
+            Console.WriteLine("Hasil penambahan 2 + 3 = {0}", cal.Penambahan(2, 3));
+            Console.WriteLine("Hasil pengurangan 7 - 3 = {0}", cal.Penambahan(7, 3));
+            Console.WriteLine("Hasil perkalian 5 x 2 = {0}", cal.Penambahan(5, 2));
+            Console.WriteLine("Hasil pembagian 6 : 2 = {0}", cal.Penambahan(6, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CalculatorMachine/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMachine
+{
+    public class Calculator
+    {
+        public int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Perkalian(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Pembagian(int a, int b)
+        {
+            return a / b;
+        }        
+    }
+}
